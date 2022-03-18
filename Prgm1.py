e698b823dc0fd3ae6ffb216f1a06818c68dae4d8
Leela05 <leelarenu05@gmail.com>
2022-03-18 15:00:15 +0530

    first commit

---
+++ Prgm1.py
@@ -0,0 +1,6 @@
+# 1.write a SQL query to delete records of those employees
+# whose salary is in the range 9000,17000
+
+# Answer:
+# DELETE FROM Employee WHERE salary BETWEEN 9000 AND 17000;
+
---
+++ Prgm2.py
@@ -0,0 +1,5 @@
+# Write an SQL query to print all Worker details from the
+# Worker table order by FIRST_NAME Ascending
+
+# Answer:
+# SELECT * FROM Worker ORDER BY FIRST_NAME ASC
